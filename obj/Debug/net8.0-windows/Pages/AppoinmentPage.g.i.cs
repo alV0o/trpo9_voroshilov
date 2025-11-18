09fbc1d2a5ba3100949173585deb2a71c02f9794
alV0o <voroshilov.olexandro07@mail.ru>
2025-11-19 00:02:02 +0500

    trpo9

--- obj/Debug/net8.0-windows/Pages/AppoinmentPage.g.i.cs
+++ obj/Debug/net8.0-windows/Pages/AppoinmentPage.g.i.cs
@@ -47,13 +47,13 @@ namespace trpo7_voroshilov_pr.Pages {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/trpo7_voroshilov_pr;V1.0.0.0;component/pages/appoinmentpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/trpo7_voroshilov_pr;component/pages/appoinmentpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\AppoinmentPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace trpo7_voroshilov_pr.Pages {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
